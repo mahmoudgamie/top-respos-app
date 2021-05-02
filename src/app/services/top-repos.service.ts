066cd96bffcd77a6f0f09b1c08813602d66cb847
Mahmoud Ismail <m.ismail@speero.net>
2021-05-02 15:16:29 +0200

    Init getTopRepos service-method

--- src/app/services/top-repos.service.ts
+++ src/app/services/top-repos.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http'
+ import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TopReposService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTopRepos() {
+    return this.http.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+  }
 }
