3bbeb22ef8ad6fe0ebd2189117344b5873f7707d
Mahmoud Ismail <m.ismail@speero.net>
2021-05-03 14:26:57 +0200

    Modify getTopRepos service method
    Add return type as observable of Response, map the the needed properties from original response

--- src/app/services/top-repos.service.ts
+++ src/app/services/top-repos.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http'
- import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Response } from '../models/response'
+import { Repo } from '../models/repo'
 
 
 @Injectable({
@@ -10,7 +13,17 @@ export class TopReposService {
 
   constructor(private http: HttpClient) { }
 
-  getTopRepos() {
-    return this.http.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+  getTopRepos(): Observable<Response> {
+    return this.http.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc').pipe(map(res => ({
+      incomplete_results: res['incomplete_results'],
+      total_count: res['total_count'],
+      items: res['items'].map(item => <Repo>({
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        stargazers_count: item.stargazers_count,
+        open_issues_count: item.open_issues_count
+      }))
+    })))
   }
 }
