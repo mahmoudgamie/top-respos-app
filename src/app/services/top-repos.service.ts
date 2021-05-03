0b72ec4aec89fe285ebcefdf01f67959c124afd8
Mahmoud Ismail <m.ismail@speero.net>
2021-05-04 01:13:09 +0200

    Modified the return of getTopRepos method

--- src/app/services/top-repos.service.ts
+++ src/app/services/top-repos.service.ts
@@ -11,21 +11,24 @@ import { Repo } from '../models/repo'
 })
 export class TopReposService {
 
+  baseUrl = 'https://api.github.com/search/repositories?q=created:>2017-10-22&per_page=10&sort=stars&order=desc';
   constructor(private http: HttpClient) { }
 
-  getTopRepos(): Observable<Response> {
-    return this.http.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc').pipe(map(res => ({
-      incomplete_results: res['incomplete_results'],
-      total_count: res['total_count'],
-      items: res['items'].map(item => <Repo>({
-        id: item.id,
-        name: item.name,
-        description: item.description,
-        stargazers_count: item.stargazers_count,
-        open_issues_count: item.open_issues_count,
-        avatar_url: item.owner.avatar_url,
-        username: item.owner.login
-      }))
-    })))
+  getTopRepos(pageNumber: number): Observable<Response> {
+    return this.http.get<Response>(this.baseUrl, { params: { page: pageNumber.toString() } })
+      .pipe(map(res => ({
+        ...res,
+        items: res.items.map(item => <Repo>({
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          stargazers_count: item.stargazers_count,
+          open_issues_count: item.open_issues_count,
+          owner: {
+            avatar_url: item.owner.avatar_url,
+            login: item.owner.login
+          }
+        }))
+      })))
   }
 }
