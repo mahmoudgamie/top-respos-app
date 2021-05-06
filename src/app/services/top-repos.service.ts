bed4989ed5345095a7633ebca8117869754af829
Mahmoud Ismail <m.ismail@speero.net>
2021-05-06 02:27:15 +0200

    Modify API to retrieve repos 30 days ago

--- src/app/services/top-repos.service.ts
+++ src/app/services/top-repos.service.ts
@@ -10,7 +10,12 @@ import { IResponse, IRepo } from '../models/response'
 })
 export class TopReposService {
 
-  baseUrl = 'https://api.github.com/search/repositories?q=created:>2017-10-22&per_page=10&sort=stars&order=desc';
+    /** @brief get date from 30 days*/
+  date = new Date();
+  targetDate = new Date(this.date.setDate(this.date.getDate() - 30))
+
+
+  baseUrl = `https://api.github.com/search/repositories?q=created:>${this.targetDate.toISOString()}&per_page=10&sort=stars&order=desc`;
   constructor(private http: HttpClient) { }
 
   /** @brief fetches top repos from API
