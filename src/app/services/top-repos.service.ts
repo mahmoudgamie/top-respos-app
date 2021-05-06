53743089242c087cc72a6aac1d3517d808244217
Mahmoud Ismail <m.ismail@speero.net>
2021-05-06 02:38:40 +0200

    Wrap owner name in strong tag

--- src/app/services/top-repos.service.ts
+++ src/app/services/top-repos.service.ts
@@ -10,10 +10,9 @@ import { IResponse, IRepo } from '../models/response'
 })
 export class TopReposService {
 
-    /** @brief get date from 30 days*/
+  /** @brief get date from 30 days*/
   date = new Date();
   targetDate = new Date(this.date.setDate(this.date.getDate() - 30))
-
 
   baseUrl = `https://api.github.com/search/repositories?q=created:>${this.targetDate.toISOString()}&per_page=10&sort=stars&order=desc`;
   constructor(private http: HttpClient) { }
